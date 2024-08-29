6070f921c01c733e6f2d920186d4bc4445cb186a
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2024-08-29 13:06:46 +0530

    updated sideBar.js file

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,64 +1,70 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import Cookies from "js-cookie";
-import homeImg from '../Images/home.png';
-import bookImg from '../Images/book.png';
-import fileImg from '../Images/file.png';
-import writingImg from '../Images/writing.png';
-import markImg from '../Images/mark.png';
-import profileImg from '../Images/profile.png';
-import logoutButtonIcon from '../Images/logout-button.png';
-import './SideBar.css';
+import homeImg from "../Images/home.png";
+import bookImg from "../Images/book.png";
+import fileImg from "../Images/file.png";
+import writingImg from "../Images/writing.png";
+import markImg from "../Images/mark.png";
+import profileImg from "../Images/profile.png";
+import logoutButtonIcon from "../Images/logout-button.png";
+import "./SideBar.css";
 
-const SideBar = ({ isAuthenticated, setIsAuthenticated, isSidebarOpen, toggleSidebar }) => {
+const SideBar = ({
+  isAuthenticated,
+  setIsAuthenticated,
+  isSidebarOpen,
+  toggleSidebar,
+}) => {
   const location = useLocation();
 
   const handleLinkClick = () => {
-    if (typeof toggleSidebar === 'function') {
-      toggleSidebar(); 
+    if (typeof toggleSidebar === "function") {
+      toggleSidebar();
     }
+  };
+
+  const handleLogout = () => {
+    console.log("Logout triggered from Sidebar");
+    console.log(`isAuthenticated: ${isAuthenticated}`);
+    Cookies.remove("userEmail");
+    setIsAuthenticated(false);
   };
   
 
-  const handleSidebarLogout = () => {
-    console.log("Logout triggered from Sidebar");
-    Cookies.remove('userEmail');
-    setIsAuthenticated(false);
-  };
-
   return (
-    <aside className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+    <aside className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
       <nav>
         <ul>
           <li className={location.pathname === "/posts" ? "active" : ""}>
             <Link to="/posts" onClick={handleLinkClick}>
-              <img className="home-img" src={homeImg} alt="homeImgIcon"/>
+              <img className="home-img" src={homeImg} alt="homeImgIcon" />
             </Link>
           </li>
           <li className={location.pathname === "/create" ? "active" : ""}>
             <Link to="/create" onClick={handleLinkClick}>
-              <img src={bookImg} alt="bookImgIcon"/>
+              <img src={bookImg} alt="bookImgIcon" />
             </Link>
           </li>
           <li className={location.pathname === "/file" ? "active" : ""}>
             <Link to="/file" onClick={handleLinkClick}>
-              <img src={fileImg} alt="fileImgIcon"/>
+              <img src={fileImg} alt="fileImgIcon" />
             </Link>
           </li>
           <li className={location.pathname === "/writing" ? "active" : ""}>
             <Link to="/writing" onClick={handleLinkClick}>
-              <img src={writingImg} alt="writingImgIcon"/>
+              <img src={writingImg} alt="writingImgIcon" />
             </Link>
           </li>
           <li className={location.pathname === "/mark" ? "active" : ""}>
             <Link to="/mark" onClick={handleLinkClick}>
-              <img src={markImg} alt="markImgIcon"/>
+              <img src={markImg} alt="markImgIcon" />
             </Link>
           </li>
           {isAuthenticated && (
             <li className={location.pathname === "/profile" ? "active" : ""}>
               <Link to="/profile" onClick={handleLinkClick}>
-                <img src={profileImg} alt="profileImgIcon"/>
+                <img src={profileImg} alt="profileImgIcon" />
               </Link>
             </li>
           )}
@@ -66,14 +72,13 @@ const SideBar = ({ isAuthenticated, setIsAuthenticated, isSidebarOpen, toggleSid
       </nav>
       {isAuthenticated && (
         <div className="sidebar-logout">
-        <img 
-          src={logoutButtonIcon} 
-          alt='logout' 
-          className='logout-button' 
-          onClick={() => handleSidebarLogout()} 
-        />
-      </div>
-      
+          <img
+            src={logoutButtonIcon}
+            alt="logout"
+            className="action-image button-logout"
+            onClick={handleLogout}
+          />
+        </div>
       )}
     </aside>
   );
