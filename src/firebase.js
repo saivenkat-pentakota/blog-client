d4ce77809788485e92c076afb447dff060b9b22e
Saivenkat Pentakota <saivenkatpentakota@gmail.com>
2025-02-19 12:01:17 +0530

    upated firebase rules

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDoEuijGGAWVUKz68IuOWt_memJStf8pn0",
+  authDomain: "blog-app-9052.firebaseapp.com",
+  projectId: "blog-app-9052",
+  storageBucket: "blog-app-9052.appspot.com",
+  messagingSenderId: "623189966746",
+  appId: "1:623189966746:web:91202942d551e4eaeb8423"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
